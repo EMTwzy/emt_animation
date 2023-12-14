a6f4715810c26f823fe47bef0c76aef11f1ed4aa
EMTwzy <emt1731041348@outlook.com>
2023-12-14 22:16:53 +0800

    home

--- src/components/HomeCompoents.vue
+++ src/components/HomeCompoents.vue
@@ -11,11 +11,17 @@
         </div>
         <!-- 随机推荐 -->
         <div class="randomVideo">
-            <h3>随机推荐</h3>
+            <div class="randomTitle">
+                <h3>随机推荐
+                    <span @click="more" class="more">查看更多</span>
+                </h3>
+            </div>
+
             <div class="random_content">
                 <div v-for="v in randomVideo" :key="v.vid" class="video-item">
                     <div class="pic">
-                        <img :src="v.vpic" alt="封面" @error="handleError" srcset="../assets/load.jpg">
+                        <img :src="v.vpic" alt="封面" v-if="v.vpic.length > 0">
+                        <img src="../assets/load.jpg" v-else-if="!v.vpic.length > 0 || v.vpic == null" alt="封面">
                         <p style="width:9rem">{{ v.vname }}</p>
                         <p>{{ v.vnote }}</p>
                     </div>
@@ -26,7 +32,14 @@
         <div class="reZero">
             <h3>Re:从零开始的异世界生活</h3>
             <div class="reThem">
-
+                <div v-for="v in reZero" :key="v.vid" class="video-item">
+                    <div class="pic">
+                        <img :src="v.vpic" alt="封面" v-if="v.vpic.length > 0">
+                        <img src="../assets/load.jpg" v-else-if="v.vpic.length == 0 || v.vpic == null" alt="封面">
+                        <p style="width:9rem">{{ v.vname }}</p>
+                        <p>{{ v.vnote }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -42,32 +55,73 @@ export default {
         const input = ref('');    //获取输入框输入数据
 
         const randomVideo = reactive([{
-            v_id: 0,//作品编号
-            v_name: '',//作品名称
-            v_state: 0,//作品状态（集数）
-            v_pic: '',//作品封面
-            v_actor: '',//声优
-            v_publishyear: 0,//上映时间（年份）
-            v_publisharea: '',//上映地区（制作国）
-            v_addtime: '',//添加时间（时间戳）
-            v_note: '',//更新状态
-            v_letter: '',//作品开头字母
-            v_director: '',//制作人
-            v_lang: '',//语种（作品语类）
-        }])
+            vid: 0,//作品编号
+            vname: '',//作品名称
+            vstate: 0,//作品状态（集数）
+            vpic: '',//作品封面
+            vactor: '',//声优
+            vpublishyear: 0,//上映时间（年份）
+            vpublisharea: '',//上映地区（制作国）
+            vaddtime: '',//添加时间（时间戳）
+            vnote: '',//更新状态
+            vletter: '',//作品开头字母
+            vdirector: '',//制作人
+            vlang: '',//语种（作品语类）
+        }]);
+        const reZero = reactive([{
+            vid: 0,//作品编号
+            vname: '',//作品名称
+            vstate: 0,//作品状态（集数）
+            vpic: '',//作品封面
+            vactor: '',//声优
+            vpublishyear: 0,//上映时间（年份）
+            vpublisharea: '',//上映地区（制作国）
+            vaddtime: '',//添加时间（时间戳）
+            vnote: '',//更新状态
+            vletter: '',//作品开头字母
+            vdirector: '',//制作人
+            vlang: '',//语种（作品语类）
+        }]);
         // 挂载完成后
         onMounted(() => {
+            //随机推荐
             axios.get("http://localhost:8080/randomVideo").then((response) => {
                 randomVideo.length = 0; // 清空数组，以便重新填充
+
                 randomVideo.push(...response.data);
-                console.log(randomVideo[0].vpic);
+                randomVideo.forEach((item) => {
+                    axios.get("http://localhost:8080/picUtils", {
+                        params: {
+                            vpic: item.vpic
+                        }
+                    }).then((response) => {
+                        console.log("test@@@@", response.data);
+                        if (!response.data)
+                            item.vpic = '';
+                    })
+                })
+            });
+            //从零主题
+            axios.get("http://localhost:8080/selectVideoByName", {
+                params: {
+                    name: '从零开始的异世界生活'
+                }
+            }).then((response) => {
+                if (response.data != null) {
+                    reZero.length = 0;
+                    reZero.push(...response.data);
+                }
             })
 
         })
+
+        function more() {
+            console.log("点击了查看更多");
+        }
         const handleError = () => {
             randomVideo.forEach((item, index) => {
                 // 直接修改数组元素以触发响应
-                randomVideo[index] = { ...item, vpic: "../assets/load.jpg" };
+                randomVideo[index] = { ...item, vpic: "" };
             });
         }
 
@@ -75,6 +129,8 @@ export default {
         return {
             input,
             randomVideo,
+            reZero,
+            more,
             handleError
         }
     }
@@ -116,31 +172,31 @@ export default {
         }
     }
 
+    /*视频模块*/
+    .video-item {
+        flex: 0 0 20%;
+        /* 让每个项目占据 20% 的宽度，一行显示 5 个项目 */
+        margin-top: 0.5rem;
+
+        .pic {
+            text-align: center;
+
+            img {
+                width: 9rem;
+                height: 11rem;
+            }
+        }
+    }
+
     /*随机推荐*/
     .randomVideo {
         margin-top: 1rem;
         width: 80%;
-
+        background: rgba(00, 00, 00, 0.7);
+        
         .random_content {
             display: flex;
             flex-wrap: wrap;
-            background: rgba(00, 00, 00, 0.7);
-
-            .video-item {
-                flex: 0 0 20%;
-                /* 让每个项目占据 20% 的宽度，一行显示 5 个项目 */
-                margin-top: 0.5rem;
-
-                .pic {
-                    text-align: center;
-                    img {
-                        width: 9rem;
-                        height: 11rem;
-                    }
-                }
-            }
-
-
             color: #fff;
         }
 
@@ -148,6 +204,34 @@ export default {
             color: white;
             margin: 0 auto;
             text-align: center;
+            padding: 1rem;
+        }
+
+        /*查看更多*/
+        .more {
+            color: red;
+            font-style: italic;
+            float: right;
+        }
+    }
+
+    /*从零模块*/
+    .reZero {
+        margin-top: 1rem;
+        width: 80%;
+        background: rgba(00, 00, 00, 0.7);
+
+        .reThem {
+            display: flex;
+            flex-wrap: wrap;
+            color: #fff;
+        }
+
+        h3 {
+            color: white;
+            margin: 0 auto;
+            text-align: center;
+            padding: 1rem;
         }
     }
 
--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 import main from '../components/MainCompoents';
 import home from '../components/HomeCompoents';
 
+
 //Vue 3 中，Vue Router 默认使用的是 HTML5 历史记录模式（history mode），这种模式下，URL 中不再带有哈希 '#'，
 const Router = createRouter({
     //告诉 Vue Router 使用浏览器的历史记录 API 来管理路由
